805f9bfb09ef49337b19b53e90aa58172d750ab7
Peter Druska <peter.druska@become.sk>
2016-03-15 10:54:09 +0100

    Table view, auto layout (https://itunes.apple.com/us/book/id1088274751)

--- MVCExample/MVCExample/ViewController.swift
+++ MVCExample/MVCExample/ViewController.swift
@@ -8,24 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-    func login(user: User) -> Bool {
+    @IBAction func register(sender: AnyObject) {
         /*
-            1) API request
+            1) API request to create new user
             2) Read response
-            3) If user is created, he is saved to persistent storage and it is returned true
+            3) If user is created on server, he is saved to persistent storage
         */
-        
-        return false
+        print("Register new user")
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("formCell", forIndexPath: indexPath) as! FormViewCell
+        if indexPath.row == 0 {
+            cell.formInput.placeholder = "Username"
+        } else if indexPath.row == 1 {
+            cell.formInput.placeholder = "Email"
+            cell.formInput.keyboardType = UIKeyboardType.EmailAddress
+        } else {
+            cell.formInput.placeholder = "Password"
+            cell.formInput.secureTextEntry = true
+        }
+        return cell
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
     }
 
 }
